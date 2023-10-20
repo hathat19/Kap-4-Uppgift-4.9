d9becf9c7343653453b18d62a5ab6c8f3a2aa492
humy06001 <humy06001@E-PW0234KS>
2023-10-20 10:05:33 +0200

    Add project files.

---
+++ Kap 4 Uppgift 4.9/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Uppgift4_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Skriv sidlängden på triangeln:");
+            int side = int.Parse(Console.ReadLine()); //Sidlängd
+
+            for (int i = 0; i < side; i++) //Hur många rader
+            {
+                
+                for (int j = 0; j <= i; j++) //hur många gånger "*" ska skrivas ut per rad
+                {
+                    Console.Write("*");
+                }
+                
+                if (i != side - 1) //ny rad efter varje rad, förutom efter den sista raden
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
